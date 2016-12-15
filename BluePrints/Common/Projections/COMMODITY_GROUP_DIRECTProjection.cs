bdf01cc6a38bf7eb8d823822e910f58daead1961
evern@outlook.com <evern@outlook.com>
2016-12-15 14:10:23 +0800

    - Before upgrading to 16.2

--- BluePrints/Common/Projections/COMMODITY_GROUP_DIRECTProjection.cs
+++ BluePrints/Common/Projections/COMMODITY_GROUP_DIRECTProjection.cs
@@ -30,6 +30,33 @@ namespace BluePrints.Common.Projections
 
         public ObservableCollection<COMMODITY_GROUP_DIRECTProjection> CHILD_COMMODITY_GROUP { get; set; }
 
+        public RATE RATE { get; set; }
+
+        public decimal ITEMRATE
+        {
+            get
+            {
+                if (RATE == null || RATE.RATE1 == null)
+                    return 0;
+
+                return (decimal)RATE.RATE1;
+            }
+        }
+
+        public decimal INSTALL_COSTS
+        {
+            get
+            {
+                if (COMMODITY_GROUP == null || COMMODITY_GROUP.HOURS_INSTALL == null)
+                    return 0;
+
+                if (RATE == null || RATE.RATE1 == null)
+                    return 0;
+
+                return (decimal)COMMODITY_GROUP.HOURS_INSTALL * (decimal)RATE.RATE1;
+            }
+        }
+
         public override string ToString()
         {
             return COMMODITY_GROUP.DESCRIPTION;
@@ -38,10 +65,11 @@ namespace BluePrints.Common.Projections
 
     public static class COMMODITY_GROUP_DIRECTProjectionQueries
     {
-        public static IQueryable<COMMODITY_GROUP_DIRECTProjection> ConvertToProjectionCOMMODITY_GROUP_DIRECT(IQueryable<COMMODITY_GROUP_DIRECT> COMMODITY_GROUP_DIRECTS)
+        public static IQueryable<COMMODITY_GROUP_DIRECTProjection> ConvertToProjectionCOMMODITY_GROUP_DIRECT(IQueryable<COMMODITY_GROUP_DIRECT> COMMODITY_GROUP_DIRECTS, Func<IQueryable<RATE>> getRATES_ByProjectFunc = null)
         {
             IEnumerable<COMMODITY_GROUP_DIRECT> allCOMMODITY_GROUP_DIRECTS = COMMODITY_GROUP_DIRECTS.ToArray().OrderBy(x => x.DESCRIPTION).AsEnumerable();
-            return allCOMMODITY_GROUP_DIRECTS.Select(x => new COMMODITY_GROUP_DIRECTProjection() { GUID = x.GUID, COMMODITY_GROUP = x }).AsQueryable();
+            IQueryable<RATE> RATES = getRATES_ByProjectFunc();
+            return allCOMMODITY_GROUP_DIRECTS.Select(x => new COMMODITY_GROUP_DIRECTProjection() { GUID = x.GUID, COMMODITY_GROUP = x, RATE = RATES.FirstOrDefault(y => y.GUID_DEPARTMENT.ToString() == CommonResources.DefaultConstructionDepartment && y.GUID_DISCIPLINE == x.GUID_DISCIPLINE) }).AsQueryable();
         }
     }
 }
