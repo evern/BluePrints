03ee22fa7e9b54ce27f05f23cb2a71f9f71a66cb
evern@outlook.com <evern@outlook.com>
2017-01-05 18:28:34 +0800

    - EntityFramework added ESTIMATION_DIRECT, ESTIMATION_DIRECT_ITEM, ESTIMATION_INDIRECT, ESTIMATION_INDIRECT_ITEM and TIMEGROUP

---
+++ BluePrints/EntityFramework/BluePrintsData/ESTIMATION_INDIRECT.cs
@@ -0,0 +1,54 @@
+namespace BluePrints.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class ESTIMATION_INDIRECT
+    {
+        public ESTIMATION_INDIRECT()
+        {
+            ESTIMATION_INDIRECT_ITEM = new HashSet<ESTIMATION_INDIRECT_ITEM>();
+        }
+
+        [Key]
+        public Guid GUID { get; set; }
+
+        public Guid GUID_PROJECT { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string NAME { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string REVISION { get; set; }
+
+        [StringLength(100)]
+        public string COMMENTS { get; set; }
+
+        public int STATUS { get; set; }
+
+        public decimal MARGIN { get; set; }
+
+        public decimal CONTINGENCY { get; set; }
+
+        public DateTime CREATED { get; set; }
+
+        public Guid CREATEDBY { get; set; }
+
+        public DateTime? UPDATED { get; set; }
+
+        public Guid? UPDATEDBY { get; set; }
+
+        public DateTime? DELETED { get; set; }
+
+        public Guid? DELETEDBY { get; set; }
+
+        public virtual ICollection<ESTIMATION_INDIRECT_ITEM> ESTIMATION_INDIRECT_ITEM { get; set; }
+
+        public virtual PROJECT PROJECT { get; set; }
+    }
+}
