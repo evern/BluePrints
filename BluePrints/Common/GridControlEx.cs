d0b6264ffca18936fe108705b9225d9993d4f6bc
Evern <evern@outlook.com>
2017-02-13 11:49:57 +0800

    - Fixed newrowitem misalignment when grid is grouped - Changed all tableview to custom table view that CommitsEditing on enter key

---
+++ BluePrints/Common/GridControlEx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media.Animation;
+using DevExpress.Xpf.Grid;
+using System.Windows.Data;
+
+namespace BluePrints.Common
+{
+    public class TableViewEx : TableView
+    {
+        public TableViewEx()
+        {
+            this.PreviewKeyDown += TableViewEx_PreviewKeyDown;
+        }
+
+        void TableViewEx_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    CommitEditing();
+                    MoveNextRow();
+                }));
+            }
+        }
+    }
+}
