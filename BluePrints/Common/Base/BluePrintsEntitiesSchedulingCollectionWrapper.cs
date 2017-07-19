de5833e1beb1404173b92b580d436b1085a43edb
Evern <evern@outlook.com>
2017-07-19 22:45:24 +0800

    - Fixed an issue where activities source isn't refreshing in SchedulingCollectionWrapper

--- BluePrints/Common/Base/BluePrintsEntitiesSchedulingCollectionWrapper.cs
+++ BluePrints/Common/Base/BluePrintsEntitiesSchedulingCollectionWrapper.cs
@@ -179,6 +179,7 @@ namespace BluePrints.Common.Base
 
         public override void FullRefresh()
         {
+            activities_source = null;
             InitializeAndLoadEntitiesLoaderDescription();
         }
 
