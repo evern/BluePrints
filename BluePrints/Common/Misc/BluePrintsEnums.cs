1958c582e5fadbbde8a53425cfe0289aff482933
Evern, Su <evern@outlook.com>
2017-10-09 15:19:11 +0800

    - Added additional type in project

--- BluePrints/Common/Misc/BluePrintsEnums.cs
+++ BluePrints/Common/Misc/BluePrintsEnums.cs
@@ -35,7 +35,9 @@ namespace BluePrints.Common
     public enum ProjectType
     {
         [Display(Name = "Design")] Design = 0,
-        [Display(Name = "Design & Construct")] DesignConstruct = 1
+        [Display(Name = "Design & Construct")] DesignConstruct = 1,
+        [Display(Name = "Study")] Study = 2,
+        [Display(Name = "Construct")] Construct = 3
     }
 
     public enum ProjectStatus
