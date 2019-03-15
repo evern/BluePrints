f1e568363f976f849c0c69608d6fe6caa0b98cdc
Evern, Su <evern@outlook.com>
2019-03-16 02:56:50 +0800

    - add a feature to allow bookable to show whether exo authorisation has been loaded

--- BluePrints/ViewModels/BASELINE_ITEM/BASELINE_ITEMCollectionViewModelWrapper.cs
+++ BluePrints/ViewModels/BASELINE_ITEM/BASELINE_ITEMCollectionViewModelWrapper.cs
@@ -1697,7 +1697,7 @@ namespace BluePrints.ViewModels
 
         public bool CanShowBookable()
         {
-            if (MainViewModel == null || DisplaySelectedEntities == null || DisplaySelectedEntities.Count() == 0)
+            if (MainViewModel == null || DisplaySelectedEntities == null || DisplaySelectedEntities.Count() == 0 || exoAuthorisations == null || narratives == null)
                 return false;
 
             return true;
