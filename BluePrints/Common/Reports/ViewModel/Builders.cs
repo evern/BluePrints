57d018de00bbde869f7e23d7313657b808d7c334
Evern, Su <evern@outlook.com>
2019-06-27 15:03:55 +0800

    - include G99 for forecast

--- BluePrints/Common/Reports/ViewModel/Builders.cs
+++ BluePrints/Common/Reports/ViewModel/Builders.cs
@@ -103,7 +103,7 @@ namespace BluePrints.Common.ViewModel.Reporting
                 {
                     if (forceRetrieveAllBurned || qualifiedSubjobs.Contains(jobTransaction.JOBCODE))
                     {
-                        if (jobTransaction.COSTDESC3 != null && (jobTransaction.COSTDESC3.Length >= 3 && (!jobTransaction.COSTDESC3.Substring(0, 3).Contains("G99") && !jobTransaction.COSTDESC3.Substring(0, 3).Contains("010"))))
+                        if (forceRetrieveAllBurned || (jobTransaction.COSTDESC3 != null && (jobTransaction.COSTDESC3.Length >= 3 && (!jobTransaction.COSTDESC3.Substring(0, 3).Contains("G99") && !jobTransaction.COSTDESC3.Substring(0, 3).Contains("010")))))
                         {
                             ExoDataPoint burnedDataPoint = new ExoDataPoint();
                             burnedDataPoint.BudgetedUnits = 0;
