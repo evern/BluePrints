46d7068e1ed86c46ab415617bf07048665d692fd
Evern, Su <evern@outlook.com>
2019-08-28 17:18:04 +0800

    - forecast job is now constructed from exo jobs only

--- BluePrints/Common/ViewModel/Misc/ForecastHelper.cs
+++ BluePrints/Common/ViewModel/Misc/ForecastHelper.cs
@@ -272,40 +272,37 @@ namespace BluePrints.Common.ViewModel.Misc
                 LoadingScreenManager.Progress();
             });
 
-            IEnumerable<Stats> actualStats = dashboardJobs.Where(x => x.Stats != null && ((SummaryStats)x.Stats).Actual != null).Select(x => ((SummaryStats)x.Stats).Actual);
-            IEnumerable<Stats> materialStats = dashboardJobs.Where(x => x.Stats != null && ((SummaryStats)x.Stats).Material != null).Select(x => ((SummaryStats)x.Stats).Material);
-            IEnumerable<Stats> poStats = dashboardJobs.Where(x => x.Stats != null && ((SummaryStats)x.Stats).PO != null).Select(x => ((SummaryStats)x.Stats).PO);
-            IEnumerable<Stats> remainingStats = dashboardJobs.Where(x => x.Stats != null && ((SummaryStats)x.Stats).Remaining != null).Select(x => ((SummaryStats)x.Stats).Remaining);
+            //IEnumerable<Stats> actualStats = dashboardJobs.Where(x => x.Stats != null && ((SummaryStats)x.Stats).Actual != null).Select(x => ((SummaryStats)x.Stats).Actual);
+            //IEnumerable<Stats> materialStats = dashboardJobs.Where(x => x.Stats != null && ((SummaryStats)x.Stats).Material != null).Select(x => ((SummaryStats)x.Stats).Material);
+            //IEnumerable<Stats> poStats = dashboardJobs.Where(x => x.Stats != null && ((SummaryStats)x.Stats).PO != null).Select(x => ((SummaryStats)x.Stats).PO);
+            //IEnumerable<Stats> remainingStats = dashboardJobs.Where(x => x.Stats != null && ((SummaryStats)x.Stats).Remaining != null).Select(x => ((SummaryStats)x.Stats).Remaining);
 
-            LoadingScreenManager.CloseLoadingScreen();
-            LoadingScreenManager.ShowLoadingScreen(1);
-            LoadingScreenManager.SetMessage("Constructing Unique Jobs from Actuals...");
-            allDataPoints.AddRange(actualStats.SelectMany(x => x.ExoDataPoints));
-            allDataPoints.AddRange(materialStats.SelectMany(x => x.ExoDataPoints));
-            allDataPoints.AddRange(poStats.SelectMany(x => x.ExoDataPoints));
+            //LoadingScreenManager.CloseLoadingScreen();
+            //LoadingScreenManager.ShowLoadingScreen(1);
+            //LoadingScreenManager.SetMessage("Constructing Unique Jobs from Actuals...");
+            //allDataPoints.AddRange(actualStats.SelectMany(x => x.ExoDataPoints));
+            //allDataPoints.AddRange(materialStats.SelectMany(x => x.ExoDataPoints));
+            //allDataPoints.AddRange(poStats.SelectMany(x => x.ExoDataPoints));
 
-            List<string> uniqueExoJobsConcatNames = dashboardJobs.Select(x => x.SubjobCode + ";" + x.DisciplineCode + ";" + x.CommodityCode + ";" + x.Variation_Code).Distinct().ToList();
+            ////List<string> uniqueExoJobsConcatNames = dashboardJobs.Select(x => x.SubjobCode + ";" + x.DisciplineCode + ";" + x.CommodityCode + ";" + x.Variation_Code).Distinct().ToList();
 
             //List<string> uniqueExoJobsConcatNames = allDataPoints.Select(x => x.Subjob_Name + ";" + x.Discipline_Code + ";" + x.Commodity_Code + ";" + x.Variation_Code).Distinct().ToList();
-            LoadingScreenManager.CloseLoadingScreen();
+            //LoadingScreenManager.CloseLoadingScreen();
 
-            LoadingScreenManager.ShowLoadingScreen(uniqueExoJobsConcatNames.Count);
-            LoadingScreenManager.SetMessage("Constructing Unique Jobs from Actuals...");
-            Parallel.ForEach(uniqueExoJobsConcatNames,
-            uniqueJobsConcatName =>
-            {
-                List<string> delimited = uniqueJobsConcatName.Split(';').ToList();
-                string subjobCode = delimited[0];
-                string disciplineCode = delimited[1];
-                string commodityCode = delimited[2];
-                string variationCode = delimited[3];
+            //LoadingScreenManager.ShowLoadingScreen(uniqueExoJobsConcatNames.Count);
+            //Parallel.ForEach(uniqueExoJobsConcatNames,
+            //uniqueJobsConcatName =>
+            //{
+            //    List<string> delimited = uniqueJobsConcatName.Split(';').ToList();
+            //    string subjobCode = delimited[0];
+            //    string disciplineCode = delimited[1];
+            //    string commodityCode = delimited[2];
+            //    string variationCode = delimited[3];
 
-                //data points from exo requires lookup and is filtered by unique code string
-                addExoSubJob(combinedSubJobs, subjobCode, disciplineCode, commodityCode, variationCode, COMMODITY_CODELookup, queriedJobs);
-                LoadingScreenManager.Progress();
-            });
-
-            int count = combinedSubJobs.Where(x => x.SubJob.Code == "03608-000-00-P1" && x.Discipline.Code == "ME01" && x.Commodity.Code == "M99").Count();
+            //    //data points from exo requires lookup and is filtered by unique code string
+            //    addExoSubJob(combinedSubJobs, subjobCode, disciplineCode, commodityCode, variationCode, COMMODITY_CODELookup, queriedJobs);
+            //    LoadingScreenManager.Progress();
+            //});
 
             LoadingScreenManager.CloseLoadingScreen();
             return combinedSubJobs.ToList();
