3893f57f9a44e28b39cb3b67615b2b098930bcd5
Evern, Su <evern@outlook.com>
2019-12-08 20:28:13 +0800

    - added forecast entities into sync context

--- BluePrints/Common/View/DXSplashScreen/SyncScreen.cs
+++ BluePrints/Common/View/DXSplashScreen/SyncScreen.cs
@@ -219,9 +219,14 @@ namespace BluePrints.Common
             qt.Add(createThread<RA_GUIDE_PROMPT>(4));
             qt.Add(createThread<RA_STUDY>(5));
             qt.Add(createThread<FORECAST>(5));
+            qt.Add(createThread<FORECAST_JOB>(6));
+            qt.Add(createThread<FORECAST_PO>(6));
             qt.Add(createThread<BASELINE>(6));
             qt.Add(createThread<DELIVERABLES_STATUS>(6));
             qt.Add(createThread<DOCTYPE>(6));
+            qt.Add(createThread<FORECAST_JOB_HOUR>(7));
+            qt.Add(createThread<FORECAST_JOB_SETTING>(7));
+            qt.Add(createThread<FORECAST_PO_SETTING>(7));
             qt.Add(createThread<PHASE>(7));
             qt.Add(createThread<SUBJOB>(7));
             qt.Add(createThread<USER>(7));
