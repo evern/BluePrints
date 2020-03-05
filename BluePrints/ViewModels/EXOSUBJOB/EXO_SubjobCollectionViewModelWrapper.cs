415eab27c5ed4df6d2720129e6ca6ceba346e64a
Evern, Su <evern@outlook.com>
2020-03-05 18:02:12 +0800

    - added summary to actual costs summary in exo jobs - fixed an issue where actual costs isn't showing because design doesn't go by stock code

--- BluePrints/ViewModels/EXOSUBJOB/EXO_SubjobCollectionViewModelWrapper.cs
+++ BluePrints/ViewModels/EXOSUBJOB/EXO_SubjobCollectionViewModelWrapper.cs
@@ -990,8 +990,8 @@ namespace BluePrints.ViewModels
             foreach (ExoSubJobEditableProjection projection in MainViewModel.Entities)
             {
                 projection.SubJobActualCostSummary = burnedDataPoints.Where(x => x.Subjob_Name == projection.SubJobCode && x.Discipline_Code == projection.DisciplineCode && x.Commodity_Code == projection.CommodityCode && x.Variation_Code == projection.VariationCode).Sum(x => x.Costs);
-                projection.SubJobMaterialCostSummary = materialDataPoints.Where(x => x.Subjob_Name == projection.SubJobCode && x.Discipline_Code == projection.DisciplineCode && x.Commodity_Code == projection.CommodityCode && x.Variation_Code == projection.VariationCode).Sum(x => x.Costs);
-                projection.SubJobRemainingPOCostSummary = materialDataPoints.Where(x => x.Subjob_Name == projection.SubJobCode && x.Discipline_Code == projection.DisciplineCode && x.Commodity_Code == projection.CommodityCode && x.Variation_Code == projection.VariationCode).Sum(x => x.Costs);
+                projection.SubJobMaterialCostSummary = materialDataPoints.Where(x => x.Subjob_Name == projection.SubJobCode && x.Discipline_Code == projection.DisciplineCode && x.Commodity_Code == projection.CommodityCode && x.StockCode == projection.StockCode && x.Variation_Code == projection.VariationCode).Sum(x => x.Costs);
+                projection.SubJobRemainingPOCostSummary = poDataPoints.Where(x => x.Subjob_Name == projection.SubJobCode && x.Discipline_Code == projection.DisciplineCode && x.Commodity_Code == projection.CommodityCode && x.StockCode == projection.StockCode && x.Variation_Code == projection.VariationCode).Sum(x => x.Costs);
             }
 
             IsActualCostSummaryVisible = true;
