5d51b73bf93746c19f7dacd19891873a123f2cdb
Evern, Su <evern@outlook.com>
2020-04-28 15:38:44 +0800

    - design timesheet scaffolding

---
+++ BluePrints/ViewModels/TIMESHEET/DesignTimesheetEntryCollectionViewModelWrapper.cs
@@ -0,0 +1,92 @@
+﻿using BaseModel.DataModel;
+using BaseModel.ViewModel.Loader;
+using BluePrints.BluePrintsEntitiesDataModel;
+using BluePrints.Common;
+using BluePrints.Common.Base;
+using BluePrints.Common.Projections;
+using BluePrints.Common.Resources;
+using BluePrints.Data;
+using BluePrints.PrimeroData;
+using BluePrints.PrimeroData.PrimeroEntitiesDataModel;
+using DevExpress.Mvvm.POCO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BluePrints.ViewModels
+{
+    public class DesignTimesheetEntryCollectionViewModelWrapper : BluePrintsEntitiesCollectionWrapper<BASELINE_ITEM, BASELINE_ITEM, Guid, IBluePrintsEntitiesUnitOfWork>
+    {
+        /// <summary>
+        /// Creates a new instance of DesignTimesheetEntryCollectionViewModelWrapper as a POCO view model.
+        /// </summary>
+        /// <param name="unitOfWorkFactory">A factory used to create a unit of work instance.</param>
+        public static DesignTimesheetEntryCollectionViewModelWrapper Create(IUnitOfWorkFactory<IBluePrintsEntitiesUnitOfWork> unitOfWorkFactory = null)
+        {
+            return ViewModelSource.Create(() => new DesignTimesheetEntryCollectionViewModelWrapper(unitOfWorkFactory));
+        }
+
+        private IPrimeroEntitiesUnitOfWork localPrimeroUnitOfWork;
+        /// <summary>
+        /// Initializes a new instance of the DesignTimesheetEntryCollectionViewModelWrapper class.
+        /// This constructor is declared protected to avoid undesired instantiation of the DesignTimesheetEntryCollectionViewModelWrapper type without the POCO proxy factory.
+        /// </summary>
+        /// <param name="unitOfWorkFactory">A factory used to create a unit of work instance.</param>
+        protected DesignTimesheetEntryCollectionViewModelWrapper(IUnitOfWorkFactory<IBluePrintsEntitiesUnitOfWork> unitOfWorkFactory = null)
+        {
+            localPrimeroUnitOfWork = PrimeroEntitiesUnitOfWorkSource.GetUnitOfWorkFactory().CreateUnitOfWork();
+        }
+
+        #region Database Operations
+        private IUnitOfWorkFactory<IPrimeroEntitiesUnitOfWork> primeroUnitOfWorkFactory = PrimeroEntitiesUnitOfWorkSource.GetUnitOfWorkFactory();
+        private IUnitOfWorkFactory<IBluePrintsEntitiesUnitOfWork> bluePrintsUnitOfWorkFactory = BluePrintsEntitiesUnitOfWorkSource.GetUnitOfWorkFactory();
+        List<ExoTimeAuthorisation> localUserExoTimeAuthorisation;
+        protected override void resolveParameters(object parameter)
+        {
+        }
+
+        protected override void addEntitiesLoader()
+        {
+        }
+
+        protected override void onAuxiliaryEntitiesCollectionLoaded()
+        {
+            CreateMainViewModel(bluePrintsUnitOfWorkFactory, x => x.BASELINE_ITEMS);
+        }
+
+        protected override Func<IRepositoryQuery<BASELINE_ITEM>, IQueryable<BASELINE_ITEM>> specifyMainViewModelProjection()
+        {
+            return query => query.Where(x => x.BASELINE.STATUS == BaselineStatus.Live);
+        }
+
+        protected override void AssignCallBacksAndRaisePropertyChange(IEnumerable<BASELINE_ITEM> entities)
+        {
+            localUserExoTimeAuthorisation = ExoQueries.GetExoLinesAuthorisations(localPrimeroUnitOfWork);
+            localUserExoTimeAuthorisation.ForEach(x => x.OfficeName = BluePrintsResources.OfficePerth);
+
+            MainViewModel.SetParentViewModel(this);
+            base.AssignCallBacksAndRaisePropertyChange(entities);
+        }
+
+        public override string UnifiedValueValidation(BASELINE_ITEM projection, string field_name, object new_value, bool isPaste)
+        {
+            return string.Empty;
+        }
+
+        public override string UnifiedRowValidation(BASELINE_ITEM projection)
+        {
+            return string.Empty;
+        }
+        #endregion
+
+        #region View Properties
+        /// <summary>
+        /// The view name to be used when saving layout for IDocumentContent
+        /// </summary>
+        public override string ViewName
+        {
+            get { return "DesignTimesheetEntryCollectionViewModelWrapper_v1"; }
+        }
+        #endregion
+    }
+}
