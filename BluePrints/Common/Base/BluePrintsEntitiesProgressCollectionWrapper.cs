d62eeea1b4492ad1267c1995380b2e0550aa3fcf
Evern, Su <evern@outlook.com>
2020-12-07 11:24:01 +0800

    - fixed pushing to P6 issue due to precision being 0.0001 now in P6 assignment

--- BluePrints/Common/Base/BluePrintsEntitiesProgressCollectionWrapper.cs
+++ BluePrints/Common/Base/BluePrintsEntitiesProgressCollectionWrapper.cs
@@ -824,8 +824,8 @@ namespace BluePrints.Common.Base
                             if (repositoryTASK.act_work_qty > 0)
                             {
                                 //assuming previous assignment is completed what the units on this deliverable should at least be
-                                decimal supposedUnitsForPreviousAssignment = deliverable.Total_Units * (assignment.LOW_VALUE - 0.01m);
-                                decimal supposedUnitsForCurrentAssignment = deliverable.Total_Units * ((assignment.HIGH_VALUE - assignment.LOW_VALUE) + 0.01m);
+                                decimal supposedUnitsForPreviousAssignment = deliverable.Total_Units * (assignment.LOW_VALUE - 0.0001m);
+                                decimal supposedUnitsForCurrentAssignment = deliverable.Total_Units * ((assignment.HIGH_VALUE - assignment.LOW_VALUE) + 0.0001m);
 
                                 //because current task can be assigned to multiple deliverable, actual earned units needs to be pro-rated
                                 decimal proRateValue = supposedUnitsForCurrentAssignment / (decimal)repositoryTASK.target_work_qty;
@@ -989,7 +989,7 @@ namespace BluePrints.Common.Base
                         if (earnedUnits >= supposedUnitsForAssignments)
                             continue;
 
-                        totalUnitsToAddToDeliverable = deliverable.Total_Units * ((completedAssignment.HIGH_VALUE - completedAssignment.LOW_VALUE) + 0.01m);
+                        totalUnitsToAddToDeliverable = deliverable.Total_Units * ((completedAssignment.HIGH_VALUE - completedAssignment.LOW_VALUE) + 0.0001m);
                     }
                     else
                         totalUnitsToAddToDeliverable = (decimal)manualParity;
@@ -1310,10 +1310,10 @@ namespace BluePrints.Common.Base
                         high_percentage_to_use = p6_assignment.HIGH_VALUE > total_percentage_to_date ? total_percentage_to_date : p6_assignment.HIGH_VALUE;
 
                     //current percentage pro-rate
-                    decimal current_percentage = p6_assignment.LOW_VALUE <= (total_percentage_to_date + 0.01m) ? ((high_percentage_to_use - p6_assignment.LOW_VALUE) + 0.01m) : 0;
+                    decimal current_percentage = p6_assignment.LOW_VALUE <= (total_percentage_to_date + 0.0001m) ? ((high_percentage_to_use - p6_assignment.LOW_VALUE) + 0.0001m) : 0;
 
                     //full assignment percentage used to calculate remaining units
-                    decimal full_assignment_percentage = ((p6_assignment.HIGH_VALUE - p6_assignment.LOW_VALUE) + 0.01m);
+                    decimal full_assignment_percentage = ((p6_assignment.HIGH_VALUE - p6_assignment.LOW_VALUE) + 0.0001m);
 
                     //current activity assignment unit
                     decimal current_assignment_units;
@@ -1335,6 +1335,10 @@ namespace BluePrints.Common.Base
                     TASK P6TASK = PROJECTTASK.FirstOrDefault(P6Task => P6Task.task_code == p6_assignment.P6_ACTIVITYID);
                     if (P6TASK != null && P6TASK.delete_date == null)
                     {
+                        string s;
+                        if(P6TASK.task_code == "A40220")
+                            s = string.Empty;
+
                         //defines how much percentage of units this assignment will take up when it is fully assigned, so that we can estimate the total duration to apply productivity to
                         decimal current_task_to_activity_percentage = (P6TASK.target_work_qty == null || P6TASK.target_work_qty == 0) ? 0 : full_assignment_units / (decimal)P6TASK.target_work_qty;
 
