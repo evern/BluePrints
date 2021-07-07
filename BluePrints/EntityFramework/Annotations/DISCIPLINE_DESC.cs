347eaf51fac8550cb61b8018d66b397df2ae658e
evern <evern@outlook.com>
2021-07-07 13:20:42 +0800

    - added package name maintenance screen

--- BluePrints/EntityFramework/Annotations/DISCIPLINE_DESC.cs
+++ BluePrints/EntityFramework/Annotations/DISCIPLINE_DESC.cs
@@ -9,7 +9,7 @@ namespace BluePrints.Data
     using System;
     using System.ComponentModel.DataAnnotations.Schema;
 
-    [ConstraintAttributes("GUID_PROJECT, NAME, DESCRIPTION")]
+    [ConstraintAttributes("NAME")]
     public partial class DISCIPLINE_DESC : EntityBase, IGuidEntityKey, ICanSync, IHaveCreatedDate
     {
         [NotMapped]
